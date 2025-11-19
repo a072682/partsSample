77d4274cfb9fa5773ad19571d1ffa4b641d10042
CubeAndy <a072682@gmail.com>
2025-11-19 13:15:30 +0800

    上傳

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/partsSample/",
+  base: process.env.NODE_ENV === 'production' ? "/partsSample/" : '/',
   plugins: [react()],
 })
